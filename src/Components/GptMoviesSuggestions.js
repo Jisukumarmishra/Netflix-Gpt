8cb91a91e415f293378a551ddbb4b2da31354b1e
mishra <jisumishra4@gmail.com>
2026-04-29 20:11:07 +0530

    Show The GptMoviesSuggestions On The Gpt page

--- src/Components/GptMoviesSuggestions.js
+++ src/Components/GptMoviesSuggestions.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import MoviesList from './MoviesList';
 
 const GptMoviesSuggestions = () => {
-  const gpt = useSelector(Store => Store.gpt); 
+  const {moviesNames, moviesResults} = useSelector((Store) => Store.gpt); 
+  if(!moviesNames) return null;
   return (
-    <div>GptMoviesSuggestions</div>
+    <div className='p-10 m-4 bg-black text-green-400'>
+     <div>
+      {moviesNames.map((moviesNames , index) => (
+        <MoviesList key="moviesNames"
+         title={moviesNames} 
+         movies={moviesResults[index]}
+         />
+        ))}
+     </div>
+    </div>
   )
 }
 
