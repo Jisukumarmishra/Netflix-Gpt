7e8bc45c073fda32aa16dd96d64e1db74ea35ee5
mishra <jisumishra4@gmail.com>
2026-04-30 19:20:31 +0530

    Polish the Ui

--- src/Components/GptMoviesSuggestions.js
+++ src/Components/GptMoviesSuggestions.js
@@ -7,13 +7,13 @@ const GptMoviesSuggestions = () => {
   const {moviesNames, moviesResults, isLoading} = useSelector((Store) => Store.gpt);
   if(!moviesNames && !isLoading) return null;
   return (
-    <div className='p-10 m-4 bg-black bg-opacity-70'>
+    <div className='p-10 m-4 bg-black bg-opacity-90'>
      <div>
       {isLoading ? (
         // show skeletons while loading
         <div>
           <h2 className="text-lg md:text-3xl py-5 md:py-6 text-white">Searching...</h2>
-          <div className="flex overflow-x-scroll">
+          <div className="flex overflow-x-scroll no-scrollbar">
             <div className="flex">
               {Array.from({length:5}).map((_,i) => <MoviesCardShimmer key={i} />)}
             </div>
